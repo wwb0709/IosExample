38a71bcab54cf43f5d8dd1dcc40c869b4e2b2fd2
wwb0709 <wwb1111@163.com>
2012-11-01 16:26:52 +0800

    videocamera

---
+++ VideoCamera/VideoCamera/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VideoCamera
+//
+//  Created by wangwb on 12-11-1.
+//  Copyright (c) 2012年 wangwb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VideoCamera2AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VideoCamera2AppDelegate class]));
+    }
+}
